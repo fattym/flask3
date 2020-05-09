fb16ac4ab4deb9fe34bea9d9e498de5d4f4e709b
fattym <jnganga288@gmail.com>
2020-05-09 14:44:10 +0300

    final commit]

--- flask3/start.sh
+++ flask3/start.sh
@@ -1,5 +1,5 @@
 export SECRET_KEY = 'c26d6bde3e77e96c634e8a88fd857560'
 export SQLALCHEMY_DATABASE_URI = 'sqlite:///site.db'
-export EMAIL_USER ='scarscoobydoo@gmail.com'
-export EMAIL_PASS ='Kenyacom23'
+export EMAIL_USER =''
+export EMAIL_PASS =''
 
